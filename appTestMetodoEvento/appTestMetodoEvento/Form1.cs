d50b598b7555b560b12c91f581920d499b3e04a4
I20050035 <70992098+I20050035@users.noreply.github.com>
2020-11-06 10:51:41 -0600

    Add files via upload

---
+++ appTestMetodoEvento/appTestMetodoEvento/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appTestMetodoEvento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void btn0_Click(object sender, EventArgs e)
+        //{
+        //    lblNumeros.Text += "0";
+
+        //}
+
+        //private void btn1_Click(object sender, EventArgs e)
+        //{
+        //    lblNumeros.Text += "1";
+        //}
+
+        private void MiMetodo_Click(object sender, EventArgs e)
+        {
+            lblNumeros.Text += ((Button)sender).Text;
+        }
+    }
+}
